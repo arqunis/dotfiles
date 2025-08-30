5416b35dbdfb8943e1918e6972ef3c34e6957bf3
Alex M. M. <acdenissk69@gmail.com>
2025-08-30 02:54:26 +0200

    ZSH: Show the current Git branch if CWD is in a Git repository

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,3 +1,17 @@
 # "LIGHTGREEN<username>RESET@<hostname> GREEN<current-directory>RESET> "
 
-PROMPT=$'%{\e[38;5;10m%}%n%f@%M %F{green}%~%f> '
+prompt_git_branch() {
+    autoload -Uz vcs_info
+    precmd_vcs_info() { vcs_info }
+    precmd_functions+=( precmd_vcs_info )
+    setopt prompt_subst
+    zstyle ':vcs_info:git:*' formats '%b'
+}
+
+prompt_git_status() {
+    [[ -n "${vcs_info_msg_0_}" ]] && echo " (${vcs_info_msg_0_})"
+}
+
+prompt_git_branch
+
+PROMPT=$'%{\e[38;5;10m%}%n%f@%M %F{green}%~%f$(prompt_git_status)> '
