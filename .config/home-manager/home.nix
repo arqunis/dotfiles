8fa84e07f95373204af552e4af0a053b35dcfec0
Alex M. M. <acdenissk69@gmail.com>
2025-10-23 23:39:22 +0200

    Manage Clangd through Home Manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -52,13 +52,19 @@
     syncthing
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".config/clangd/config.yaml".text = ''
+    InlayHints:
+      Enabled: No
+
+    ---
+
+    If:
+      PathMatch: [.*\.h, .*\.hpp]
+
+    CompileFlags:
+      Add: [-Wno-unused-function, -Wno-unused-macros]
+    '';
   };
 
   home.sessionVariables = {
