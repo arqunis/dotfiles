65c41afe5ca6e643c85e4c0f0188e86817a37d8c
Alex M. M. <acdenissk69@gmail.com>
2025-10-25 23:33:21 +0200

    Manage Neovim through Home Manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -25,8 +25,6 @@
 
     fastfetch
 
-    neovim
-
     hledger
     hledger-ui
     hledger-web
@@ -59,17 +57,6 @@
 
   home.file = {
   };
-
-  home.sessionVariables = (
-    let
-      editor = "nvim";
-    in
-    {
-      EDITOR = editor;
-      GIT_EDITOR = editor;
-      VISUAL = editor;
-    }
-  );
 
   home.sessionPath = [
     "$HOME/.local/bin"
@@ -208,6 +195,72 @@
       '';
     in
       lib.mkMerge [ earlyInit afterCompletion config ];
+  };
+
+  programs.neovim = {
+    enable = true;
+
+    defaultEditor = true;
+
+    extraLuaConfig = lib.fileContents ./nvim.lua;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      (nvim-treesitter.withPlugins (p: [
+        p.asm
+        p.bash
+        p.c
+        p.c_sharp
+        p.cmake
+        p.cpp
+        p.css
+        p.d
+        p.dockerfile
+        p.go
+        p.hare
+        p.haskell
+        p.html
+        p.ini
+        p.java
+        p.javascript
+        p.json
+        p.kotlin
+        p.latex
+        p.ledger
+        p.lua
+        p.make
+        p.markdown
+        p.markdown-inline
+        p.meson
+        p.nginx
+        p.nim
+        p.nix
+        p.odin
+        p.php
+        p.python
+        p.query
+        p.robots
+        p.ruby
+        p.rust
+        p.ssh_config
+        p.svelte
+        p.swift
+        p.toml
+        p.tsx
+        p.typescript
+        p.typst
+        p.vim
+        p.vimdoc
+        p.vue
+        p.xml
+        p.xml
+        p.yaml
+        p.zig
+      ]))
+    ];
   };
 
   programs.git = {
