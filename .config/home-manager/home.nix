24cfe26c972b55554cb793c73d5cea3338853204
Alex M. M. <acdenissk69@gmail.com>
2025-10-27 00:27:40 +0100

    Manage GTK configurations through Home Manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -309,6 +309,30 @@
     ];
   };
 
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
